bcc98adeb02d11c8f0f59643168a2f9af749002b
Awaddd <34748292+Awaddd@users.noreply.github.com>
2025-07-27 14:30:57 +0100

    Improve product card

--- components/Product.tsx
+++ components/Product.tsx
@@ -12,16 +12,13 @@ type Props = {
 export default function ({ id = "product", product, className }: Props) {
     return (
         <Link href={`/products/${product.id}`}>
-            <div
-                id={id}
-                className={cn("rounded-lg", className)}
-            >
+            <div id={id}>
                 <MotionImage
                     src={product.imageUrl}
                     alt={`${product.name} ${product.category}`}
-                    className="w-full h-48 xl:h-48 object-cover rounded-t-lg"
+                    className={cn("w-full h-48 xl:h-48 object-cover rounded-lg", className)}
                 />
-                <div className="py-4 px-2">
+                <div className="py-2 px-2 bg-background">
                     <h5 className="mt-2 font-medium">{product.name}</h5>
                     <p className="text-muted-foreground">£{product.price}</p>
                 </div>
